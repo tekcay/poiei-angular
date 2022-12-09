776fef9755a1455c1f7a3cf4b4f880df1a1f2f08
tekcay <pierreblc@hotmail.fr>
2022-12-09 13:47:31 +0100

    question 13

--- src/app/pet/pet-list/pet-list-item/pet-list-item.component.ts
+++ src/app/pet/pet-list/pet-list-item/pet-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { IPet } from '../../model/pet';
+import { PetService } from '../../pet.service';
 
 @Component({
   selector: 'app-pet-list-item',
@@ -9,6 +10,9 @@ import { IPet } from '../../model/pet';
 export class PetListItemComponent {
   @Input() pet!: IPet;
 
-  constructor() {}
+  constructor(private petService: PetService) {}
 
+  get selectedPetId(): number {
+    return this.petService.selectedPet ? this.petService.selectedPet.id : 0;
+  }
 }
