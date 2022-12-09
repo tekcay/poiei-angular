b57221c115ace2781c50420f6d16b0d125c38f68
tekcay <pierreblc@hotmail.fr>
2022-12-09 13:58:23 +0100

    quesiton 15: added a picture for each pet

--- src/app/pet/pet.service.ts
+++ src/app/pet/pet.service.ts
@@ -23,9 +23,22 @@ export class PetService {
     const species: Species[] = ['chien', 'chat', 'poisson', 'lapin'];
     const isAvailables: boolean[] = [true, false, true, false];
     const prices: number[] = [500, 400, 10, 50];
+    const imagesURL: string[] = [
+      'https://cdn001.tintin.com/public/tintin/img/static/milou/milou_v3.png',
+    'https://upload.wikimedia.org/wikipedia/en/thumb/b/bc/Garfield_the_Cat.svg/1200px-Garfield_the_Cat.svg.png',
+    'https://easydrawingguides.com/wp-content/uploads/2017/05/How-to-Draw-Nemo-20.png',
+    'https://img.src.ca/2015/07/27/1250x703/150727_co2q2_aetd_bugs-bunny_sn1250.jpg'
+  ];
     
     for (let i = 0; i < names.length; i++) {
-      const pet = {id: i + 1, name: names[i], species: species[i], isAvailable: isAvailables[i], price: prices[i]};
+      const pet = {
+        id: i + 1,
+         name: names[i],
+          species: species[i],
+           isAvailable: isAvailables[i],
+            price: prices[i],
+            imageUrl: imagesURL[i]
+          };
       this.pets.push(pet);
     }   
   }
